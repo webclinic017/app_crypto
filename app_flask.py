f2ccdd13e81eabceb62891960800a88a8d55788d
fforner <80565354+fforner@users.noreply.github.com>
2021-09-17 14:44:40 -0400

    Update app_flask.py

--- app_flask.py
+++ app_flask.py
@@ -9,4 +9,4 @@ def home():
 if __name__ == '__main__':
     app.jinja_env.auto_reload = True
     app.config["TEMPLATES_AUTO_RELOAD"] = True
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
